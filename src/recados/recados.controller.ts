109b1a53cbfbb47b812c4aadf415fe42a6e00d05
enzomelomartins <enzofullstack@gmail.com>
2026-02-23 18:13:19 -0300

    creating others methods and set pagination

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
   @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
-    return 'todos os recados foram listados com sucesso.';
+  findAll(@Query() pagination: any) {
+  const { limit = 10, offset = 0 } = pagination;
+    return `Retorna todos os recados. Limit=${limit}, Offset=${offset}`;
   }
 
   @Get(':id')
@@ -16,5 +17,18 @@ export class RecadosController {
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  @Patch(':id')
+  update (@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body
+    }
+  }
+  
+  @Delete(':id')
+  remove (@Param('id') id: string) {
+    return `Essa rota APAGA o recado ID ${id}`;
   }
 }
