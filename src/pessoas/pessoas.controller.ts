33bfa7c49cd2494cf52556d2b1aa13d183d864f7
enzomelomartins <enzofullstack@gmail.com>
2026-04-08 16:40:41 -0300

    injection fake token to server name

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject,  } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { RecadosUtils } from 'src/recados/recados.utils';
+import { SERVER_NAME } from 'src/common/constants/server-name.constant';
 
 @Controller('pessoas')
 export class PessoasController {
   constructor(
     private readonly pessoasService: PessoasService,
     private readonly recadosUtils: RecadosUtils,
+    @Inject(SERVER_NAME)
+    private readonly nomeDaVariavel: string,
   ) { }
 
   @Post()
@@ -18,7 +21,7 @@ export class PessoasController {
 
   @Get()
   findAll() {
-    console.log(this.recadosUtils.inverteString('Luiz'));
+    console.log(this.nomeDaVariavel);
     return this.pessoasService.findAll();
   }
 
