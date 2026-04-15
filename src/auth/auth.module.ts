cfc589d3f3bda00b26a8a3e0e9c221bcf61d4209
enzomelomartins <enzofullstack@gmail.com>
2026-04-15 17:23:23 -0300

    adding login route and generate accessToken with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pessoa } from 'pessoas/entities/pessoa.entity';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config'
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Pessoa]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   controllers: [AuthController],
   providers: [
     {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from '/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { access } from 'fs';
 
 @Injectable()
 export class AuthService {
-  async login(loginDto: LoginDto) {
-    return loginDto;
-  }
+    constructor(
+        @InjectRepository(Pessoa)
+        private readonly pessoaRepository: Repository<Pessoa>,
+        private readonly hashingService: HashingService,
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+        private readonly jwtService: JwtService,
+    ) { }
+
+    async login(loginDto: LoginDto) {
+        const pessoa = await this.pessoaRepository.findOneBy({
+            email: loginDto.email,
+        });
+
+        if (!pessoa) {
+            throw new UnauthorizedException('Pessoa não existe.');
+        }
+
+        const passwordIsValid = await this.hashingService.compare(
+            loginDto.password,
+            pessoa.passwordHash,
+        );
+
+        if (!passwordIsValid) {
+            throw new UnauthorizedException('Senha inválida!');
+        }
+
+        const accessToken = await this.jwtService.signAsync(
+            {
+                sub: pessoa.id,
+                email: pessoa.email,
+            },
+            {
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+                secret: this.jwtConfiguration.secret,
+                expiresIn: this.jwtConfiguration.jwtTtl,
+            },
+        );
+
+        return {
+            accessToken,
+        };
+    }
 }
